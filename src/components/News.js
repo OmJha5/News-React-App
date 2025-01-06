980ec0ff40ed60fc48b81fb27ea7d7f46c5e0d04
om <omjha003@gmail.com>
2025-01-06 16:08:17 +0530

    Fetching API keys from News API

--- src/components/News.js
+++ src/components/News.js
@@ -5,12 +5,26 @@ export default class News extends Component {
   render() {
     return (
       <div>
-        This is a news Component
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
+
+        <div className="container my-3">
+            <h1 className='my-5'>NewsShorts -- Top Headlines!</h1>
+            
+            <div className="row">
+                <div className="col-md-4">
+                    <NewsItems title="Criket" description="This is Nice" />
+                </div>
+
+                <div className="col-md-4">
+                    <NewsItems title="Movie" description="This is Movie" />
+                </div>
+
+                <div className="col-md-4">
+                    <NewsItems title="Politics" description="This is Politics" />
+                </div>
+
+            </div>
+        </div>
+
       </div>
     )
   }
